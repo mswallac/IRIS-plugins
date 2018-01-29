db5474b66b2e529abfb5ac2a0673b37e27a4c26c
mswallac <mswallac@bu.edu>
2018-01-29 17:03:44 -0500

    Large changes to overall plugin structure to conserve memory and reduce redundancy
    Also adds more options to menu and begins implementation of PMMA and H2O RI calculations.

--- generateLUT/src/main/java/irisFun2.java
+++ generateLUT/src/main/java/irisFun2.java
@@ -1,6 +1,5 @@
 import org.apache.commons.math3.analysis.differentiation.FiniteDifferencesDifferentiator;
 import org.apache.commons.math3.analysis.differentiation.UnivariateDifferentiableVectorFunction;
-import org.apache.commons.math3.complex.Complex;
 import org.apache.commons.math3.fitting.leastsquares.MultivariateJacobianFunction;
 import org.apache.commons.math3.linear.Array2DRowRealMatrix;
 import org.apache.commons.math3.linear.ArrayRealVector;
@@ -10,19 +9,20 @@ import org.apache.commons.math3.stat.StatUtils;
 import org.apache.commons.math3.util.FastMath;
 import org.apache.commons.math3.util.Pair;
 
-public class irisFun implements MultivariateJacobianFunction {
+public class irisFun2 implements MultivariateJacobianFunction {
 	public int t;
-	public double temp;
+	public double thickness;
 	public IrisUtils iu;
 	
-	public irisFun(IrisUtils in1,double temp) {
+	public irisFun2(IrisUtils in1,double temp,double d) {
 		iu=in1;
 		t=(int)temp;
+		thickness=d;
 	}
 	
 	public Pair<RealVector, RealMatrix> value(RealVector in) {
 		double[] din = in.toArray(),y = new double[4];
-		y = irisfxn(din[0], din[1], din[2]);
+		y = irisfxn(thickness, din[0], din[1]);
 		
 		final RealVector ydata = new ArrayRealVector(y);
 		final RealMatrix jacobian = getJacobian(din);
@@ -40,9 +40,9 @@ public class irisFun implements MultivariateJacobianFunction {
 			int ct=0;
 			for(double i=.4;i<.651;i+=0.001){
 				sirefract=iu.interpolateSI(i);
-				sirefract2=iu.SiRI(i,temp);
-				filmr=iu.getFilm(i,temp);
-				medr=iu.getMedium(i,temp);
+				sirefract2=iu.SiRI(i,t);
+				filmr=iu.getFilm(i,t);
+				medr=iu.getMedium(i,t);
 				rsivalue=(iu.fresnel(medr,medr,sirefract,start,i));
 				rvalue=(iu.fresnel(medr,filmr,sirefract2,start,i));
 				s=(iu.interpolateLED(j,i));
@@ -65,17 +65,15 @@ public class irisFun implements MultivariateJacobianFunction {
 	}
 	
 	public RealMatrix getJacobian(double[] in) {
-		irisD irisd = new irisD(iu,t,in[1],in[2]);
-		irisM irism = new irisM(iu,t,in[0],in[2]);
-		irisB irisb = new irisB(iu,t,in[0],in[1]);
-		FiniteDifferencesDifferentiator diff = new FiniteDifferencesDifferentiator(5, .25);
-		UnivariateDifferentiableVectorFunction irisdd = diff.differentiate(irisd),
-				irisdm = diff.differentiate(irism), irisdb = diff.differentiate(irisb);
-		double[] dvals = irisdd.value(in[0]),mvals = irisdm.value(in[1]),bvals = irisdb.value(in[2]);
-		final RealMatrix jacobian = new Array2DRowRealMatrix(4,3);
-		jacobian.setColumn(0, dvals);
-		jacobian.setColumn(1, mvals);
-		jacobian.setColumn(2, bvals);
+		irisM irism = new irisM(iu,t,thickness,in[1]);
+		irisB irisb = new irisB(iu,t,thickness,in[0]);
+		FiniteDifferencesDifferentiator diff = new FiniteDifferencesDifferentiator(3, .25);
+		UnivariateDifferentiableVectorFunction irisdm = diff.differentiate(irism);
+		UnivariateDifferentiableVectorFunction irisdb = diff.differentiate(irisb);
+		double[] mvals = irisdm.value(in[0]),bvals = irisdb.value(in[1]);
+		final RealMatrix jacobian = new Array2DRowRealMatrix(4,2);
+		jacobian.setColumn(0, mvals);
+		jacobian.setColumn(1, bvals);
 		return jacobian;
 		
 	}
---
+++ generateLUT/src/main/java/waterRI.java
@@ -0,0 +1,59 @@
+import org.apache.commons.math3.analysis.UnivariateFunction;
+import org.apache.commons.math3.util.FastMath;
+
+public class waterRI implements UnivariateFunction {
+double a0,a1,a2,a3,a4,a5,a6,a7,lambda_uv,lambda_ir,T_ref,p_ref,lambda_ref,T_bar,p,p_bar,lambda_bar,luv2,lir2,n2,lbar2,pbar2;
+double[] wtemps={0,4,10,20,30,40,50,60,70,80,90,100},plook={999.8,1000,999.7,998.2,995.7,992.2,988.1,983.2,977.8,971.8,965.3,958.4};
+	
+	public waterRI(double temp,double lambda) {
+		a0 = .2442577733;
+		a1 = 9.74634476e-3;
+		a2 = -3.73234996e-3;
+		a3=2.68678472e-4;
+		a4=1.58920570e-3;
+		a5 = 2.45934259e-3;
+		a6 = .900704920;
+		a7 = -1.66626219e-2;
+		lambda_uv=.2292020;
+		lambda_ir=5.432937;
+		
+		T_ref = 273.15;
+		p_ref = 1000;
+		lambda_ref = .589;
+		
+		T_bar = (temp+T_ref)/T_ref;
+		p = interpolate2(wtemps,plook, temp, 12);
+		p_bar = p/p_ref;
+		lambda_bar = lambda/lambda_ref;
+	}
+
+	public double value(double n) { 
+		luv2 = FastMath.pow(lambda_uv, 2);
+		lir2 = FastMath.pow(lambda_ir, 2);
+		n2 = FastMath.pow(n, 2);
+		lbar2 = FastMath.pow(lambda_bar, 2);
+		pbar2 = FastMath.pow(p_bar, 2);
+		double result = ((((n2-1)/(n2+2))/p_bar)-(a0+a1*p_bar+a2*T_bar+a3*(lbar2)*T_bar+a4/lbar2+a5/(luv2)+a6/(lbar2-lir2)+a7*pbar2));
+		return result;
+	}
+	
+	public double interpolate2(double data1[],double data2[],double input, int size){
+		int ind=-1;
+
+		for(int i=0;i<=size-1;i++){
+			if(input>data1[i] && input<data1[i+1])
+				ind=i;
+		}
+
+		if(ind!=-1){
+			double x1 = data1[ind];
+			double x2 = data1[ind+1];
+			double y1 = data2[ind];
+			double y2 = data2[ind+1];
+			return ((((y2-y1)/(x2-x1))*(input-x1))+y1);
+		}else {
+			return 0;
+		}
+	}
+
+}
